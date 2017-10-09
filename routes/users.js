cef579939aebc8ea8f54a6aa3b0a905a65315012
barisariburnu <barisariburnu@gmail.com>
2017-10-09 22:24:33 +0300

    Add user model

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // Register
-router.get('/register', (req, res, next) => {
-    res.send("REGISTER");
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({
+        success: false,
+        msg: 'Failed to register user'
+      });
+    } else {
+      res.json({
+        success: true,
+        msg: 'User registered'
+      });
+    }
+  })
 });
 
 // Login
-router.get('/login', (req, res, next) => {
-    res.send("LOGIN");
+router.post('/login', (req, res, next) => {
+  res.send("LOGIN");
 });
 
 // Profile
 router.get('/profile', (req, res, next) => {
-    res.send("PROFILE");
+  res.send("PROFILE");
 });
 
 module.exports = router;
